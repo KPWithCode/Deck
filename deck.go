cdb3c559d068a223c4e8602fae3a9123397dd5b7
Kyle Phillips <kphillips375@yahoo.com>
2020-10-31 13:58:39 -0400

    created a new deck function

--- deck.go
+++ deck.go
@@ -5,6 +5,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Aces","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven"}
+
+	// anytime you have a variable you're not using you replace
+	// with an underscore (i,j)
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+ " of "+ value)
+		}
+	}
+	return cards
+}
+
 //  receiver function
 func (d deck) print() {
 for i, card := range d {
