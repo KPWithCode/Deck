f31cd9fcd47359be9ca9686266df2f8488f627d6
Kyle Phillips <kphillips375@yahoo.com>
2020-10-31 21:36:52 -0400

    deal function returns two values

--- deck.go
+++ deck.go
@@ -1,22 +1,22 @@
 package main
 
 import "fmt"
-// Create type of deck 
+
+// Create type of deck
 // which is a slice of strings
 type deck []string
-
 
 func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Aces","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven"}
+	cardValues := []string{"Aces", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven"}
 
 	// anytime you have a variable you're not using you replace
 	// with an underscore (i,j)
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, suit+ " of "+ value)
+			cards = append(cards, suit+" of "+value)
 		}
 	}
 	return cards
@@ -24,7 +24,13 @@ func newDeck() deck {
 
 //  receiver function
 func (d deck) print() {
-for i, card := range d {
-	fmt.Println(i, card)
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
 }
+
+// deal cards
+// returns multiple values 
+func deal(d deck, handSize int)(deck, deck) {
+	return d[:handSize], d[handSize:]
 }
